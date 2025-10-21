07759b9d58d75618fb3469aae94a1d127aac8693
samuelsmp03 <samuelmpereira13@gmail.com>
2025-10-21 09:03:07 -0300

    Listar, pegar e soltar

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 import mapa
-import mensagens
+from mensagens import imprimirDescricaoSala, imprimirDestinos, imprimirItens,imprimirInventario
+from player import pegarItem, soltarItem,getInventario, getTamInv
 
 directions = {"north", "south", "east", "west"}
 
 while True:
     nome = mapa.get_current_room_name()
     print(f"[{nome}]\n")
-    mensagens.imprimirDescricaoSala(mapa.get_description())
+    imprimirDescricaoSala(mapa.get_description())
 
     exits = mapa.get_exits()
     if exits:
         for d, r in exits.items():
-            mensagens.imprimirDestinos(d, r)
+            imprimirDestinos(d, r)
 
     itens = mapa.get_items()
     if itens:
-        mensagens.imprimirItens(itens)
+        imprimirItens(itens)
 
     if mapa.is_exit():
         print("\nVocê encontrou a saída! Parabéns!")
         break
 
-    move = input("\nDigite a direção (north, south, east, west) ou 'sair' para encerrar: \n> ").lower().strip()
-    if move == "sair":
+    movimento = input("\nVocê deseja ir para uma direção(direcao), realizar uma ação relacionada a item(acao) ou sair do jogo(sair)? \n> ").lower().strip()
+    if movimento == "sair":
         print("Jogo encerrado.")
         break
+    elif movimento == 'direcao':
+        movimento = input("\nDigite a direção (north, south, east, west): ").lower().strip()
+        if movimento in directions:
+            if not mapa.move(movimento):
+                print("Direção inválida! Tente novamente.")
 
-    if move in directions:
-        if not mapa.move(move):
-            print("Direção inválida! Tente novamente.")
+    elif movimento == 'acao':
+        acao_item = input("\nVocê deseja 'pegar' ou 'soltar' um item ou 'listar' seu inventário? ").lower().strip()
+        if acao_item == 'pegar':
+            item_nome = input("Digite o nome do item que deseja pegar: ").strip()
+            if not pegarItem(item_nome):
+                print("\nNão foi possível pegar o item :(. Verifique se ele está na sala ou se há espaço no inventário.\n")
+            else:
+                print(f"\nVocê pegou o item: {item_nome}!!!")
+
+        elif acao_item == 'soltar':
+            item_nome = input("\nDigite o nome do item que deseja soltar: ").strip()
+            if item_nome in getInventario():
+                item_desc = getInventario()[item_nome]
+                soltarItem(item_nome,item_desc)
+                print(f"Você soltou o item: {item_nome}")
+            else:
+                print("Item não encontrado no inventário.")
+                continue
+
+        elif acao_item == 'listar':
+            imprimirInventario()
+
+        else:
+            print("Comando inválido.")
     else:
         print("Comando inválido.")
--- mensagens.py
+++ mensagens.py
@@ -1,5 +1,5 @@
 import random
-
+from player import getInventario, getTamInv
 #Traduzir direções para português
 def retornaDirecaoPT(direcao:str):
     if direcao == "north":
@@ -55,13 +55,27 @@ def imprimirItens(itens:dict):
     if not itens:
         return
     for nome_item, desc in itens.items():
-        nome_item = nome_item.capitalize()
+        nome_item = nome_item
         desc = desc.capitalize()
         num_random = random.choice([1, 2, 3])
         if num_random == 1:
-            print(f"Você observa um{verificaPalavraFeminina(nome_item)}{nome_item}, ao se aproximar você nota mais detalhes:{desc}")
+            print(f"Você observa um{verificaPalavraFeminina(nome_item)}[{nome_item}], ao se aproximar você nota mais detalhes:{desc}")
         elif num_random == 2:
-            print(f"Um{verificaPalavraFeminina(nome_item)}{nome_item} está aqui. Chegando mais perto, você nota alguns detalhes: {desc}")
+            print(f"Um{verificaPalavraFeminina(nome_item)}[{nome_item}] está aqui. Chegando mais perto, você nota alguns detalhes: {desc}")
         else:
-            print(f"Há um{verificaPalavraFeminina(nome_item)}{nome_item} neste local. Vendo com calma, você percebe que é um(a): {desc}")
+            print(f"Há um{verificaPalavraFeminina(nome_item)}[{nome_item}] neste local. Vendo com calma, você percebe que é um(a): {desc}")
+    return
+
+def imprimirInventario():
+    inventario = getInventario()
+    tam_inv = getTamInv()
+    if tam_inv == 0:
+        print("Seu inventário está vazio.")
+        return
+    print("Itens no seu inventário:")
+    print(f"\nSeu inventário possui {tam_inv} itens")
+    for nome_item, desc in inventario.items():
+        nome_item = nome_item
+        desc = desc.capitalize()
+        print(f"- [{nome_item}]: {desc}")
     return
